8c0ee12d6999bc5517b7bf319a060ce67c1017b8
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2015-10-25 14:13:54 +0530

    WIP - can create png file from mermaid script

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var pandoc=require('pandoc-filter');
 var _ = require('lodash');
+var tmp = require('tmp');
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
 
+var prefix="diagram-";
+var outdir= "img"
+var mermaid_opts = "-v -o " + outdir;
+var cmd = "mermaid " + mermaid_opts;
+var counter = 0;
 function mermaid(type, value, format, meta) {
     if (type != "CodeBlock")
         return null
@@ -8,7 +17,24 @@ function mermaid(type, value, format, meta) {
     var attrs = value[0]
     var classes = attrs[1]
     if (_.contains('mermaid', classes)) {
+        counter++;
         console.log(content);
+        tmp.file(function(err, tmpfile, fd, cleanup) {
+           if(err) throw err;
+           console.log(tmpfile);
+           fs.writeFile(tmpfile, content, function(err) {
+               if(err) throw err;
+               exec(cmd + " " + tmpfile, function(err, stdin, stdout) {
+                if (err) throw err;
+                console.log(stdout);
+                var oldPath = path.join(outdir, path.basename(tmpfile) + ".png"); 
+                var newPath = path.join(outdir, prefix + counter + ".png");
+                console.log(oldPath, newPath);
+                fs.renameSync(oldPath, newPath);
+               });
+           });
+        });
+
     }
     return null;
 
