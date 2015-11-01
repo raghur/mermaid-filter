0403be7aaef56d22719d9838371b8294a1d20f8a
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2015-11-01 19:25:01 +0530

    clean up code with some es2015 bits; remove dependencies

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #! /usr/bin/env node
 var pandoc=require('pandoc-filter');
-var _ = require('lodash');
 var tmp = require('tmp');
 var fs = require('fs');
 var path = require('path');
@@ -13,24 +12,26 @@ var cmd = "mermaid " + mermaid_opts;
 var counter = 0;
 function mermaid(type, value, format, meta) {
     if (type != "CodeBlock") return null;
-    var content=value[1];
-    var attrs = value[0];
+    var attrs = value[0],
+        content = value[1];
     var classes = attrs[1];
     var options = {width: '500', format: 'png'};
-    _.forEach(attrs[2], function(item) {
+    if (classes.indexOf('mermaid') < 0) return null;
+
+    counter++;
+    attrs[2].map(item => {
         if (item.length  === 1) options[item[0]] = true;
         else options[item[0]] = item[1];
     });
-    console.log(options);
-    if (!_.contains('mermaid', classes)) return null;
-    counter++;
+    //console.log(options);
     //console.log(content);
     var tmpfileObj = tmp.fileSync();
     //console.log(tmpfile);
     fs.writeFileSync(tmpfileObj.name, content);
-    var oldPath = path.join(outdir, path.basename(tmpfileObj.name) + "." + options.format); 
-    var newPath = path.join(outdir, prefix + counter + "." + options.format);
-    exec(cmd + " -w " + options.width + " " + (options.format==='png' ? "-p": "-s") + " " + tmpfileObj.name);
+    var oldPath = path.join(outdir, path.basename(tmpfileObj.name) + `.${options.format}`);
+    var newPath = path.join(outdir, `${prefix}${counter}.${options.format}`);
+    exec(`${cmd} -w ${options.width} ${options.format==='png' ? "-p": "-s"}  ${tmpfileObj.name}`);
+    //console.log(oldPath, newPath);
     fs.renameSync(oldPath, newPath);
     return pandoc.Para([pandoc.Image([],[newPath, ""])]);
 }
