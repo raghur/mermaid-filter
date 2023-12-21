ede9146c6471e6091d26970cd9e51137e2c3a472
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2023-12-21 14:09:45 +0530

    WIP: add unit tests

---
+++ index.test.js
@@ -0,0 +1,22 @@
+var utils = require('./lib')
+describe('external tool lookup', () => {
+    function findTool(name, env) {
+        return utils.externalTool(name, env, () => fail(`expected to find utility ${name}`));
+    }
+    it ('should find mmdc tool', () => {
+        findTool('mmdc');
+    })
+    it ('should find imgur tool', () => {
+        findTool('imgur');
+    })
+    describe('env overrides', () => {
+        it ('should allow tool override from env', () => {
+            let path = findTool('mmdc', { 'MERMAID_FILTER_CMD_MMDC': '/usr/bin/ls' });
+            expect(path).toEqual('"/usr/bin/ls"');
+        })
+        it ('should only override where env key matches', () => {
+            let path = findTool('imgur', { 'MERMAID_FILTER_CMD_MMDC': '/usr/bin/ls' });
+            expect(path).not.toEqual('"/usr/bin/ls"');
+        })
+    })
+})
