a4697b7f9273e3a8b1e5c9a82793fb1bf5d5800e
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2023-12-23 10:18:18 +0530

    fix: eslint checks

--- index.test.js
+++ index.test.js
@@ -1,22 +1,23 @@
-var utils = require('./lib')
+/* global describe it expect fail */
+const utils = require('./lib')
 describe('external tool lookup', () => {
-    function findTool(name, env) {
-        return utils.externalTool(name, env, () => fail(`expected to find utility ${name}`));
-    }
-    it ('should find mmdc tool', () => {
-        findTool('mmdc');
+  function findTool (name, env) {
+    return utils.externalTool(name, env, () => fail(`expected to find utility ${name}`))
+  }
+  it('should find mmdc tool', () => {
+    findTool('mmdc')
+  })
+  it('should find imgur tool', () => {
+    findTool('imgur')
+  })
+  describe('env overrides', () => {
+    it('should allow tool override from env', () => {
+      const path = findTool('mmdc', { MERMAID_FILTER_CMD_MMDC: '/usr/bin/ls' })
+      expect(path).toEqual('"/usr/bin/ls"')
     })
-    it ('should find imgur tool', () => {
-        findTool('imgur');
+    it('should only override where env key matches', () => {
+      const path = findTool('imgur', { MERMAID_FILTER_CMD_MMDC: '/usr/bin/ls' })
+      expect(path).not.toEqual('"/usr/bin/ls"')
     })
-    describe('env overrides', () => {
-        it ('should allow tool override from env', () => {
-            let path = findTool('mmdc', { 'MERMAID_FILTER_CMD_MMDC': '/usr/bin/ls' });
-            expect(path).toEqual('"/usr/bin/ls"');
-        })
-        it ('should only override where env key matches', () => {
-            let path = findTool('imgur', { 'MERMAID_FILTER_CMD_MMDC': '/usr/bin/ls' });
-            expect(path).not.toEqual('"/usr/bin/ls"');
-        })
-    })
+  })
 })
